92992a898cbbff6e0cae3238c1bfe0fa1aa0d99c
Elton Luiz <elton.unix@gmail.com>
2023-08-14 14:24:56 -0300

    implementando Pageable para paginação.

--- src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.filter.LancamentoFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 
 public interface LancamentoRepositoryQuery {
 
-    public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+    public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 
 }
