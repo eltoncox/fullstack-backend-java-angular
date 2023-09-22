68fd9d54994e2ea7103ff5b42797aa3c748710b2
Elton Luiz <elton.unix@gmail.com>
2023-09-22 17:11:43 -0300

    Ajustando o CEP, atualização da classe pessoa.

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package com.example.algamoney.api.resource;
 
-
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +12,8 @@ import com.example.algamoney.api.service.PessoaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -70,7 +72,6 @@ public class PessoaResource {
         return ResponseEntity.ok(pessoaSalva);
     }
 
-
     @PutMapping("/{codigo}/ativo")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and hasAuthority('SCOPE_write')")
@@ -78,5 +79,10 @@ public class PessoaResource {
         pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
     }
 
+    @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA')")
+    public Page<Pessoa> pesquisar(@RequestParam(required = false, defaultValue = "") String nome, Pageable pageable) {
+        return pessoaRepository.findByNomeContaining(nome, pageable);
+    }
 
 }
